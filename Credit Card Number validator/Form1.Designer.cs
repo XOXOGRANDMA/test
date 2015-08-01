15aeace53ac694d1234eec45ce6450732810692d
XOXOGRANDMA <Patrick_sessa@live.com>
2015-08-01 15:47:02 -0400

    Credit Card Validator
    Generates credit card numbers then compares them to the luhn algorithm

---
+++ Credit Card Number validator/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Credit_Card_Number_validator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.generateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.discoverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.masterCardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Card number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(12, 54);
+            this.maskedTextBox1.Mask = "0000000000000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(115, 20);
+            this.maskedTextBox1.TabIndex = 3;
+            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            this.maskedTextBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.maskedTextBox1_MouseClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.generateToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(247, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // generateToolStripMenuItem
+            // 
+            this.generateToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.visaToolStripMenuItem,
+            this.discoverToolStripMenuItem,
+            this.masterCardToolStripMenuItem});
+            this.generateToolStripMenuItem.Name = "generateToolStripMenuItem";
+            this.generateToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.generateToolStripMenuItem.Text = "Generate";
+            this.generateToolStripMenuItem.Click += new System.EventHandler(this.generateToolStripMenuItem_Click);
+            // 
+            // visaToolStripMenuItem
+            // 
+            this.visaToolStripMenuItem.Name = "visaToolStripMenuItem";
+            this.visaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.visaToolStripMenuItem.Text = "Visa";
+            this.visaToolStripMenuItem.Click += new System.EventHandler(this.visaToolStripMenuItem_Click);
+            // 
+            // discoverToolStripMenuItem
+            // 
+            this.discoverToolStripMenuItem.Name = "discoverToolStripMenuItem";
+            this.discoverToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.discoverToolStripMenuItem.Text = "Discover";
+            this.discoverToolStripMenuItem.Click += new System.EventHandler(this.discoverToolStripMenuItem_Click);
+            // 
+            // masterCardToolStripMenuItem
+            // 
+            this.masterCardToolStripMenuItem.Name = "masterCardToolStripMenuItem";
+            this.masterCardToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.masterCardToolStripMenuItem.Text = "MasterCard";
+            this.masterCardToolStripMenuItem.Click += new System.EventHandler(this.masterCardToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 86);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Credit Card Validator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem generateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem discoverToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem masterCardToolStripMenuItem;
+    }
+}
+
---
+++ Credit Card Number validator/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Credit_Card_Number_validator
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Todo: only display numbers that pass the luhn algorithm if generated
+        /// Todo: look into CVV algorithm
+        /// Todo: bogus address creator
+        /// Todo: bogus phone number generator
+        /// Todo: bogus name generator
+        /// Todo: Display bogus "identity" in separate form. Read-only.
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool Mod10Check(string creditCardNumber)
+        {
+
+            int sumOfDigits = 0;
+            int index = 0;
+            foreach (int digit in creditCardNumber.Where(e => e >= '0' && e <= '9').Reverse())
+            {
+                int temp = (index % 2 == 0) ? digit - 48 : (digit - 48) * 2;
+                sumOfDigits += temp < 10 ? temp : temp - 9;
+                index++;
+            }
+
+            return sumOfDigits % 10 == 0;
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+            if (maskedTextBox1.Text.Length < 16)
+            {
+                MessageBox.Show("Card number not long enough","Invalid card number");
+
+            }
+            else
+            {
+                string a = maskedTextBox1.Text;
+                bool isValid = Mod10Check(a);
+                if (isValid == true)
+                {
+                    label1.Text = "Valid";
+                }
+                else
+                {
+                    label1.Text = "Invalid";
+                }
+
+            }
+                 
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            
+        }
+
+        private void maskedTextBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            maskedTextBox1.SelectionStart = 0;
+        }
+        long longRandom(long min, long max, Random rand)
+        {
+            byte[] buf = new byte[8];
+            rand.NextBytes(buf);
+            long longRand = BitConverter.ToInt64(buf, 0);
+
+            return (Math.Abs(longRand % (max - min)) + min);
+        }
+        private void generateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void visaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                long r = longRandom(Math.Abs(4000000000000000), Math.Abs(4999999999999999), new Random());
+                maskedTextBox1.Text = r.ToString();
+            }
+            catch (Exception vex)
+            {
+                MessageBox.Show(vex.ToString());
+            }
+        }
+
+        private void discoverToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                long r = longRandom(Math.Abs(6000000000000000), Math.Abs(6999999999999999), new Random());
+                maskedTextBox1.Text = r.ToString();
+            }
+            catch (Exception exd)
+            {
+                MessageBox.Show(exd.ToString());
+            }
+
+        }
+
+        private void masterCardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                long r = longRandom(Math.Abs(5000000000000000), Math.Abs(5999999999999999), new Random());
+                maskedTextBox1.Text = r.ToString();
+            }
+            catch (Exception mex)
+            {
+                MessageBox.Show(mex.ToString());
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
